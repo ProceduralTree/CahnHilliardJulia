5530e382cfb4dc94e505fce90d1ccb616b430533
Jonathan Ulmer <jonathan.ulmer@ulmail.de>
2024-03-06 12:29:37 +0100

    cleanup

--- utils.jl
+++ utils.jl
@@ -223,3 +223,15 @@ function set_xi_and_psi!(solver::T) where T <: Union{multi_solver , relaxed_mult
     solver.psi[2:end-1, 2:end-1] = psi_init.(solver.phase[2:end-1,2:end-1])
     return nothing
 end
+
+function bulk_energy(solver::T) where T <: Union{multi_solver , relaxed_multi_solver}
+    energy = 0
+    dx = CartesianIndex(1,0)
+    dy = CartesianIndex(0,1)
+    W(x) = 1/4 * (1-x^2)^2
+    for I in CartesianIndices(solver.phase)[2:end-1,2:end-1]
+        i,j = I.I
+        energy += solver.epsilon^2 / 2 * G(i+ 0.5,j ,solver.len, solver.width) * (solver.phase[I+dx] - solver.phase[I])^2 + G(i,j+0.5,solver.len ,solver.width) * (solver.phase[I+dy] - solver.phase[I])^2 + W(solver.phase[I])
+        end
+   return energy
+end
