9b874e8e79e87b89887124bc8b240d17c6eea878
John <jonathan.ulmer@ulmail.de>
2024-03-30 12:17:10 +0100

    multithreaded testing

--- src/optimize-alpha.jl
+++ src/optimize-alpha.jl
@@ -13,19 +13,8 @@ using Distributed
 M = jldopen("../data/test-phasefield.jld2")["M"]
 
 original_grid = testgrid(multi_solver, M, 2)
-alphas = 1e4:1e3:1e6
-
-
-set_xi_and_psi!(original_grid[1])
-for j in 1:10
-    v_cycle!(original_grid, 1)
-end
-tasks = Set{Task}()
-for α in alphas
-    t = Threads.@spawn alpha_error(α , original_grid[1].phase)
-    push!(tasks , t)
-end
-results  = [fetch(t) for t in tasks]
+alphas = 0:1e3:1e6
+length(alphas)
 
 function alpha_error(α::Number , solution::Array )
     test_solver  = testgrid(relaxed_multi_solver, M, 2, alpha=α)
@@ -36,3 +25,14 @@ function alpha_error(α::Number , solution::Array )
     end
 return norm(test_solver[1].phase - solution)
 end
+
+set_xi_and_psi!(original_grid[1])
+for j in 1:10
+    v_cycle!(original_grid, 1)
+end
+tasks = []
+for α in alphas
+    t = Threads.@spawn alpha_error(α , original_grid[1].phase)
+    push!(tasks , (α=α , task = t))
+end
+results  = @show [(alpha=t.α, error=fetch(t.task)) for t in tasks]
