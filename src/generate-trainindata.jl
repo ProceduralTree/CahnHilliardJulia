dde23720aa33b3e70bf78c3709ae707b9fb37f03
Jonathan Ulmer <jonathan.ulmer@ulmail.de>
2024-04-02 13:10:43 +0200

    finished traindata generator

--- src/generate-trainindata.jl
+++ src/generate-trainindata.jl
@@ -11,51 +11,45 @@ using LinearAlgebra
 using ProgressBars
 using Random
 using DataFrames
-SIZES = [2^i for i = 5:9]
-SIZE = SIZES[2]
-M = fill(-1, SIZE, SIZE)
+
 incirc(M) = filter(x -> norm(x.I .- (size(M, 1) / 2, size(M, 2) / 2)) < min(size(M)...) / 5, CartesianIndices(M))
 insquare(M) = filter(x -> norm(x.I .- (size(M, 1) / 2, size(M, 2) / 2), Inf) < min(size(M)...) / 4, CartesianIndices(M))
-side(M) = filter(x -> x < CartesianIndex(size(M, 1), size(M, 2) ÷ 2), CartesianIndices(M))
-halfcirc(M) = filter(x -> norm(x.I .- (1, size(M, 2) / 2), 2) < 12, CartesianIndices(M))
-M[insquare(M)] .= 1
-display(heatmap(M, aspect_ratio=:equal, xlims=(1, SIZE)))
-t = 50
-M = testdata(64, 8, 8, 2)
+side(M) = filter(x -> x.I[2] < size(M, 2) ÷ 2, CartesianIndices(M))
+halfcirc(M) = filter(x -> norm(x.I .- (1, size(M, 2) / 2), 2) < min(size(M)...) / 5, CartesianIndices(M))
 
-trainings_input = [
-    [testdata(i, 8, 8, j) for i = 2 .^ (5:9), j in [2, Inf]];
-    [do M=fill(-1, s, s)
-        M[incirc(M)] .= 1
-        end  for s = 2 .^ (5:9)
-    ]
-    [do M=fill(-1, s, s)
-        M[insquare(M)] .= 1
-        end  for s = 2 .^ (5:9)
-    ]
-    [do M=fill(-1, s, s)
-        M[side(M)] .= 1
-        end  for s = 2 .^ (5:9)
-    ]
-    [do M=fill(-1, s, s)
-        M[halfcirc(M)] .= 1
-        end  for s = 2 .^ (5:9)
-    ]
-]
-
-
-jldsave("data/test-phasefield.jld2"; M=g[1].phase[2:end-1, 2:end-1])
+function get_special_input(fn, size)
+    M = fill(-1, size , size )
+    M[fn(M)] .= 1
+    return M
+end
 
 function gen_cycle_data(M::Array)
     out = []
-    iter = 2 .^ (0:7)
+    iter = 2 .^ (0:6)
     g = testgrid(multi_solver, M, 2)
     set_xi_and_psi!(g[1])
     for i = 1:128
         v_cycle!(g, 1)
         if i in iter
-            push!(out, (phase=g[1].phase, iteration=i))
+            push!(out, (phase=copy(g[1].phase), iteration=i))
         end
     end
+    out = [merge(o,(;expected=g[1].phase)) for o in out]
     return out
 end
+
+display(heatmap(M, aspect_ratio=:equal, xlims=(1, SIZE)))
+
+t1= [testdata(i, i ÷ 4, i /4 , j) for i = 2 .^ (5:9), j in [2, Inf]]
+t2 = [get_special_input(fn,s) for s = 2 .^ (5:9), fn in [halfcirc , incirc, side , insquare]]
+trainings_input = reshape([t1;; t2], *(size([t1;; t2])...))
+tasks = []
+for i in eachindex(trainings_input)
+    t = Threads.@spawn gen_cycle_data(trainings_input[i])
+    push!(tasks , (iteration = 1 , task = t))
+    end
+result = DataFrame()
+for task in tasks
+    append!(result , fetch(task) )
+    end
+jldsave("data/trainings_data.jld2"; result)
